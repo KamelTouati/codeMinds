407d1dbeae2f8402015a69bc9b92bb62d80d4010
ACHREF999 <achrefdouda1@gmail.com>
2023-12-15 11:01:35 +0100

    Added Comments to the schedule schema and components for the logged in users

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,17 +2,11 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import {
   Landing,
-  Quiz,
-  UserSearch,
   Login,
-  Fields,
   Register,
   Profile,
-  CreateProject,
-  ProjectDetails,
   ResetPassword,
   ResetPasswordConfirm,
-  Activate,
 } from "./containers";
 
 import Layout from "./hocs/Layout";
@@ -21,6 +15,7 @@ import store from "./store";
 
 import AOS from "aos";
 import "aos/dist/aos.css"; // You can also use <link> for styles
+import AdminLanding from "./containers/admin/AdminLanding";
 // ..
 AOS.init();
 
@@ -29,22 +24,25 @@ export default function App() {
     <Provider store={store}>
       <Router>
         <Layout>
+          l;kpok
           <div className="container">
             <Routes>
               <Route path="/" exact element={<Landing />} />
               <Route path="/login" exact element={<Login />} />
-              <Route path="/fields" exact element={<Fields />} />
+              {/* <Route path="/fields" exact element={<Fields />} /> */}
               <Route path="/register" exact element={<Register />} />
-              <Route path="/quiz" exact element={<Quiz />} />
+              {/* <Route path="/quiz" exact element={<Quiz />} /> */}
               <Route path="/profile" exact element={<Profile />} />
-              <Route path="/create-project" exact element={<CreateProject />} />
-              <Route path="/user-search" exact element={<UserSearch />} />
-              <Route path="/activate" exact element={<Activate />} />
-              <Route
+              <Route path="/admin" exact element={<AdminLanding />} />
+              <Route path="/dashboard" exact element={<AdminLanding />} />
+              {/* <Route path="/create-project" exact element={<CreateProject />} /> */}
+              {/* <Route path="/user-search" exact element={<UserSearch />} /> */}
+              {/* <Route path="/activate" exact element={<Activate />} /> */}
+              {/* <Route
                 path="/project-details"
                 exact
                 element={<ProjectDetails />}
-              />
+              /> */}
               <Route path="/reset-password" exact element={<ResetPassword />} />
               <Route
                 path="/reset-password-confirm"
---
+++ frontend/src/containers/admin/AdminLanding.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Sidebar from '../../components/Sidebar'
+
+function AdminLanding() {
+  return (
+    <div>
+        
+        jajaga
+
+    </div>
+  )
+}
+
+export default AdminLanding
